a1782cb22230ac760086991adaacc8cdda19d39a
sujal22311521-alt <sujal.22311521@viit.ac.in>
2026-05-03 21:52:10 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is a leading online platform that provides a wide range of plants 
+        and gardening products. Our mission is to bring nature closer to people by offering 
+        high-quality plants and an easy shopping experience.
+      </p>
+      <p>
+        We aim to promote greenery and sustainability by making plants accessible to everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
